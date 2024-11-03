f28b427882580cd88a1e5fe39864bdd64eb077b3
HP <pouryazeidi@yahoo.com>
2024-11-03 16:25:54 +0300

    fixed fav icone -title on document-logo on public file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,14 @@
+// src/pages/_document.tsx
+
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Pourya Zeydi</title>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
