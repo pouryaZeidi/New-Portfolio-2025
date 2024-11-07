da73df9906628cc7e8254b5df525c0ea619d7492
HP <pouryazeidi@yahoo.com>
2024-11-07 12:41:38 +0300

    remove chakra because of have to install framer motion -

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,15 +1,14 @@
 import Welcome from '@/components/Welcome'
 import WelcomeText from '@/components/WelcomeText'
-import { Box } from '@chakra-ui/react'
 import React from 'react'
 
 const Layout = ({ children }: { children: any }) => {
   return (
-    <Box position="relative" bg="black" w="100%" h="100vh" overflow="hidden">
+    <div className='relative bg-black w-full h-[100vh] overflow-hidden '>
       <Welcome />
       <WelcomeText />
       {children}
-    </Box>
+    </div>
   )
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,12 @@
-
-import { ThemeProvider } from "next-themes";
 import { AppProps } from "next/app";
 import Layout from "@/layout/layout";
-import Provider from "@/components/Provider";
-import { defaultSystem , ChakraProvider } from '@chakra-ui/react';
 import 'tailwindcss/tailwind.css'; 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    // <ThemeProvider attribute="class" disableTransitionOnChange>
-      <ChakraProvider value={defaultSystem}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
-      </ChakraProvider>
-    // </ThemeProvider>
   );
 }
 
