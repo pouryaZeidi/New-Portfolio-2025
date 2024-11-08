255058aa2295809d2f5bf9dcb4d9569634745188
HP <pouryazeidi@yahoo.com>
2024-11-08 18:37:29 +0300

    build navbar items and logo and navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import NavbarItems from './NavbarItems'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='fixed top-0 left-0 w-full  h-24 flex items-center justify-between px-6  z-50'>
+      <img src="/Logo.png" alt="Logo" className='w-[80px] bg-white rounded-full' />
+      <div className='w-[70%] p-4 bg-black bg-opacity-70  backdrop-blur-2xl shadow-lg rounded-2xl flex items-center justify-between m-auto'>
+      <NavbarItems href={'/'} txt={'About'}/>
+      <NavbarItems href={'/'} txt={'Projects'}/>
+      <NavbarItems href={'/'} txt={'On Versel'}/>
+      </div>
+      </div>
   )
 }
 
