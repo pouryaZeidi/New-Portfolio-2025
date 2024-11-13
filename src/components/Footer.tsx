66fd2ce0c3c3643271e36ba72618d9e5eedc9d29
HP <pouryazeidi@yahoo.com>
2024-11-13 21:48:24 +0300

    some changes on navbar footer and add resume downloader btn on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,73 +3,92 @@ import Link from 'next/link';
 import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
-  return (
-    <footer className="relative z-50 bg-gray-900 text-white p-10">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-start">
-        {/* Logo and Description */}
-        <div className="mb-5 md:mb-0 md:w-1/3">
-          <h1 className="text-2xl font-bold">
-            <span className="text-yellow-300">Pourya</span> Zeydi
-          </h1>
-          <p className="mt-2 text-gray-400 text-sm">
-            Frontend Developer | Passionate about building responsive and user-friendly web applications. Let’s connect and create something amazing!
-          </p>
-        </div>
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = '/PouryaCV.pdf';  // مسیر فایل رزومه
+    link.download = 'PouryaCV.pdf'; // نام فایل دانلود
+    link.click();
+  };
 
-        {/* Social Links */}
-        <div className="flex flex-col items-start md:items-center md:w-1/3">
-          <h2 className="text-lg font-semibold mb-3">Follow Me</h2>
-          <div className="flex space-x-4">
-            <Link
-              href="https://github.com/pouryaZeidi"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="group text-2xl transition-transform transform cursor-pointer hover:scale-110 hover:text-yellow-300"
-            >
-              <FaGithub className="group-hover:drop-shadow-[0_0_10px_rgb(234,179,8)]" />
-            </Link>
-            <Link
-              href="https://www.instagram.com/mit_poriz/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="group text-2xl transition-transform transform hover:scale-110 hover:text-yellow-300"
-            >
-              <FaInstagram className="group-hover:drop-shadow-[0_0_10px_rgb(234,179,8)]" />
-            </Link>
-            <Link
-              href="https://www.linkedin.com/in/pourya-zeydi-b6b440260/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="group text-2xl transition-transform transform hover:scale-110 hover:text-yellow-300"
-            >
-              <FaLinkedin className="group-hover:drop-shadow-[0_0_10px_rgb(234,179,8)]" />
-            </Link>
+  return (
+    <footer className="relative z-50 bg-gray-950  text-white p-10">
+      <div className=' p-5'>
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-start">
+          {/* Logo and Description */}
+          <div className="mb-5 md:mb-0 md:w-1/3">
+            <h1 className="text-2xl font-bold">
+              <span className="text-yellow-300">Pourya</span> Zeydi
+            </h1>
+            <p className="mt-2 text-gray-400 text-sm">
+              Frontend Developer | Passionate about building responsive and user-friendly web applications. Let’s connect and create something amazing!
+            </p>
+          </div>
+
+          {/* Social Links */}
+          <div className="flex flex-col items-start md:items-center md:w-1/3">
+            <h2 className="text-lg font-semibold mb-3">Follow Me</h2>
+            <div className="flex space-x-4">
+              <Link
+                href="https://github.com/pouryaZeidi"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group text-2xl transition-transform transform cursor-pointer hover:scale-110 hover:text-yellow-300"
+              >
+                <FaGithub className="group-hover:drop-shadow-[0_0_10px_rgb(234,179,8)]" />
+              </Link>
+              <Link
+                href="https://www.instagram.com/mit_poriz/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group text-2xl transition-transform transform hover:scale-110 hover:text-yellow-300"
+              >
+                <FaInstagram className="group-hover:drop-shadow-[0_0_10px_rgb(234,179,8)]" />
+              </Link>
+              <Link
+                href="https://www.linkedin.com/in/pourya-zeydi-b6b440260/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group text-2xl transition-transform transform hover:scale-110 hover:text-yellow-300"
+              >
+                <FaLinkedin className="group-hover:drop-shadow-[0_0_10px_rgb(234,179,8)]" />
+              </Link>
+            </div>
+          </div>
+
+          {/* Contact Info */}
+          <div className="mt-5 md:mt-0 md:w-1/3">
+            <h2 className="text-lg font-semibold mb-3">Contact</h2>
+            <p className="text-gray-400 text-sm">
+              Email:{" "}
+              <Link
+                href="mailto:zeidipourya@gmail.com"
+                className="hover:text-yellow-300 transition-colors"
+              >
+                zeidipourya@gmail.com
+              </Link>
+            </p>
+            <p className="text-gray-400 text-sm">Location: Antalya, Turkey</p>
           </div>
         </div>
 
-        {/* Contact Info */}
-        <div className="mt-5 md:mt-0 md:w-1/3">
-          <h2 className="text-lg font-semibold mb-3">Contact</h2>
-          <p className="text-gray-400 text-sm">
-            Email:{" "}
-            <Link
-              href="mailto:zeidipourya@gmail.com"
-              className="hover:text-yellow-300 transition-colors"
-            >
-              zeidipourya@gmail.com
-            </Link>
-          </p>
-          <p className="text-gray-400 text-sm">Location: Antalya, Turkey</p>
+        {/* Divider */}
+        <div className="border-t border-gray-700 mt-10"></div>
+
+        {/* Download Resume Button */}
+        <div className="mt-5 text-center">
+          <button
+            onClick={handleDownload}
+            className="text-gray-600 bg-yellow-300 hover:bg-yellow-600 py-2 px-4 rounded-md shadow-md transform transition-all duration-300 ease-in-out hover:scale-105"
+          >
+            Download PDF Resume
+          </button>
         </div>
-      </div>
 
-      {/* Divider */}
-      <div className="border-t border-gray-700 mt-10"></div>
-
-      {/* Copyright */}
-      <div className="mt-5 text-center text-sm text-gray-500">
-        © 2024 Pourya Zeydi. All rights reserved. | Built with{" "}
-        <span className="text-red-500">❤</span> using Next.js & Tailwind CSS
+        {/* Copyright */}
+        <div className="mt-5 text-center text-sm text-gray-500">
+          © 2024 Pourya Zeydi. All rights reserved. | Built with{" "}
+          <span className="text-red-500">❤</span> using Next.js & Tailwind CSS
+        </div>
       </div>
     </footer>
   );
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,13 +7,13 @@ const Navbar = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-24 flex items-center justify-between px-6 z-50">
       <img src="/Logo.png" alt="Logo" className="w-[80px] bg-white rounded-full" />
-      <div className="hidden sm:flex w-[70%] p-4 bg-black bg-opacity-70 border-white border-[1px] backdrop-blur-2xl shadow-lg rounded-2xl items-center justify-between m-auto">
+      <div className="hidden sm:flex w-[70%] p-4 shadow-custombox  bg-black bg-opacity-70 border-white border-[1px] backdrop-blur-2xl  rounded-2xl items-center justify-between m-auto">
         <NavbarItems href={'/'} txt={'About Me'} />
         <NavbarItems href={'/'} txt={'Projects Codes'} />
         <NavbarItems href={'/'} txt={'On Versel'} />
       </div>
-      <div className="sm:hidden">
-        <Hamburger toggled={isOpen} toggle={setIsOpen} size={24} color='white' />
+      <div className="sm:hidden ">
+        <Hamburger toggled={isOpen} toggle={setIsOpen} size={24} color='white'  />
         {isOpen && (
           <div className="absolute top-24 right-6 w-[150px] h-[200px] bg-black bg-opacity-90 border-white border-[1px] backdrop-blur-2xl shadow-lg rounded-2xl flex flex-col justify-between items-center p-4">
             <NavbarItems href={'/'} txt={'About Me'} />
