5253efdb3444044b3c75429b84a22c74b77fd0ee
HP <pouryazeidi@yahoo.com>
2024-11-26 22:06:10 +0300

    remove the welcome component get hide

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+// import React from 'react';
 
-const Welcome: React.FC = () => {
+// const Welcome: React.FC = () => {
 
-  return (
-    <div className=' hidden  absolute top-0 left-0 w-full sm:flex justify-center items-center h-screen z-10'>
-      <div className='animate-leftWindow delay-[5000ms] w-full h-screen flex justify-center items-center origin-left bg-black text-white'>
-      </div>
-      <div className=' animate-leftWindow delay-[5000ms] w-full h-screen flex justify-center items-center origin-right bg-black text-white'>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className=' hidden  absolute top-0 left-0 w-full sm:flex justify-center items-center h-screen z-10'>
+//       <div className='animate-leftWindow delay-[5000ms] w-full h-screen flex justify-center items-center origin-left bg-black text-white'>
+//       </div>
+//       <div className=' animate-leftWindow delay-[5000ms] w-full h-screen flex justify-center items-center origin-right bg-black text-white'>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Welcome;
+// export default Welcome;
--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,13 +1,13 @@
 import Footer from '@/components/Footer'
-import Welcome from '@/components/Welcome'
-import WelcomeText from '@/components/WelcomeText'
+// import Welcome from '@/components/Welcome'
+// import WelcomeText from '@/components/WelcomeText'
 import React from 'react'
 
 const Layout = ({ children }: { children: any }) => {
   return (
     <div className='relative bg-black w-full h-[100vh] overflow-hidden '>
-      <Welcome />
-      <WelcomeText />
+      {/* <Welcome />
+      <WelcomeText /> */}
       {children}
       <Footer/>
     </div>
